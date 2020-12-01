1ff233526a926f6695d2505ef4900c8b3c76e85d
Hyeeun Lee <21545@student.dorset-college.ie>
2020-12-01 13:13:13 +0000

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int people = 90;
+        int maxPeople = 600;
+        int min = 0;
+
+        do {
+            people = people - 40  + 65 ;
+            min = min+5;
+            System.out.printf("After %d minutes - total people is %d\n", min, people);
+
+        } while (people < maxPeople);
     }
 }
